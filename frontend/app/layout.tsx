7c0ac37515d757412a8006771736caf2b8964444
Kiss Tamás <kiss.tamas.j@gmail.com>
2025-11-14 23:05:10 +0100

    feat: create nextjs frontend with shadcn ui

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Health and Fitness App",
+  description:
+    "Track your workouts, monitor your progress, and achieve your fitness goals with our comprehensive health and fitness application.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
