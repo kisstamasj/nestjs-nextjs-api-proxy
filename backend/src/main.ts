115c7c4467272e64c84bdcd2e95acab47f045839
Kiss Tamás <kiss.tamas.j@gmail.com>
2025-11-16 22:39:01 +0100

    refactor(cors): remove commented code for CORS credentials

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS with credentials to allow cookies
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
