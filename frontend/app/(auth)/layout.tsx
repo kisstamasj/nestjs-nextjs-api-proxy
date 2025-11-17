adf3b3e9f2a1220f92dcc834fcbe762d31bb1e92
Kiss Tamás <kiss.tamas.j@gmail.com>
2025-11-17 11:56:01 +0100

    refactor(auth): simplify layout in SignIn and SignUp pages

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -12,5 +12,9 @@ export default async function AuthLayout({
   if (session) {
     redirect("/");
   }
-  return <>{children}</>;
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">{children}</div>
+    </div>
+  );
 }
