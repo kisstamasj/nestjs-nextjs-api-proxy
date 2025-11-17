5f6967d796f8fc303eb69b8f21f89623aae9b823
Kiss Tamás <kiss.tamas.j@gmail.com>
2025-11-18 00:17:05 +0100

    fix(config): correct the order of SESSION_SECRET export in config file

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -10,8 +10,8 @@ if (!process.env.NEXT_PUBLIC_APP_URL) {
 
 export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 export const BACKEND_API_URL = process.env.BACKEND_API_URL;
+export const SESSION_SECRET = process.env.SESSION_SECRET;
 export const SIGN_IN_ENDPOINT = "/auth/sign-in";
 export const SIGN_OUT_ENDPOINT = "/auth/sign-out";
 export const REFRESH_ENDPOINT = "/auth/refresh";
-export const SESSION_SECRET = process.env.SESSION_SECRET;
 export const SESSION_TOKEN_COOKIE = "session_token";
