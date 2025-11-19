c9e7db737894e435a6b80ec5124fefeceaa3068c
Kiss Tamás <kiss.tamas.j@gmail.com>
2025-11-19 09:26:05 +0100

    feat(session): enhance session management with dynamic expiration and remove token utility

--- frontend/lib/session.ts
+++ frontend/lib/session.ts
@@ -1,8 +1,9 @@
-"use server";
+
 
 import { JWTPayload, jwtVerify, SignJWT } from "jose";
+import { ResponseCookies } from "next/dist/compiled/@edge-runtime/cookies";
 import { cookies } from "next/headers";
-import { SESSION_SECRET, SESSION_TOKEN_COOKIE } from "./config";
+import { SESSION_EXPIRES_IN, SESSION_SECRET, SESSION_TOKEN_COOKIE } from "./config";
 
 
 const encodedKey = new TextEncoder().encode(SESSION_SECRET);
@@ -19,13 +20,14 @@ export interface User {
 export interface SessionPayload extends JWTPayload, User {
   accessToken: string;
   refreshToken: string;
+  rememberMe: boolean;
 }
 
 export async function encrypt(payload: SessionPayload) {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("7d")
+    .setExpirationTime(payload.rememberMe ? new Date(Date.now() + SESSION_EXPIRES_IN) : new Date(Date.now() + 60 * 60 * 1000))
     .sign(encodedKey);
 }
 
@@ -48,10 +50,27 @@ export async function getUserSession() {
   }
   const session = await decrypt(sessionToken);
 
-  if(!session) {
+  if (!session) {
     return null;
   }
 
   const { accessToken, refreshToken, ...user } = session;
   return user as User;
+}
+
+export function getSessionTokenOption(token: string, expires?: Date) {
+  return {
+    name: SESSION_TOKEN_COOKIE,
+    value: token,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax" as const,
+    path: "/",
+    expires,
+  };
+}
+
+export async function setSessionCookie(cookie: ResponseCookies, session: SessionPayload, expires?: Date) {
+  const encryptedSession = await encrypt(session);
+  cookie.set(getSessionTokenOption(encryptedSession, expires));
 }
